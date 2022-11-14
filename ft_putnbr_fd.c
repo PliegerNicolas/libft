c426e58cad69072e51158adaa1e54f6f62011ebb
Nicolas PLIEGER <nplieger@e3r11p8.clusters.42paris.fr>
2022-11-14 14:55:13 +0100

    Removing ft_itoa to prevent prohibited malloc.

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,12 +6,33 @@
 /*   By: nplieger <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 12:24:19 by nplieger          #+#    #+#             */
-/*   Updated: 2022/11/12 11:56:02 by nplieger         ###   ########.fr       */
+/*   Updated: 2022/11/14 14:53:03 by nplieger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	if (n == -2147483648)
+	{
+		ft_putnbr_fd((n / 10), fd);
+		ft_putnbr_fd(-(n % 10), fd);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			ft_putchar_fd('-', fd);
+			n = -n;
+		}
+		if (n >= 10)
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+		else
+		{
+			ft_putchar_fd(n + '0', fd);
+		}
+	}
 }
